7d4f1b88f93e5cbb0c0ef85bcb54391006882c19
Wences81 <zhylinpochta@gmail.com>
2022-06-03 14:47:35 +0200

    day1

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
 import React, { useState } from 'react';
+import PostForm from './components/PostForm';
 import PostList from './components/PostList';
-import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/input/MyInput';
+import MySelect from './components/UI/select/MySelect';
+
 import './styles/App.css';
 // import Counter from './components/Counter';
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: 1, title: 'Javascript', body: 'Description' },
-    { id: 2, title: 'Javascript2', body: 'Description' },
-    { id: 3, title: 'Javascript3', body: 'Description' },
-
-
+    { id: 1, title: 'aa', body: 'bb' },
+    { id: 2, title: 'gg2', body: 'aa' },
+    { id: 3, title: 'bb3', body: 'yy' },
   ])
+
+  const [selectedSort, setSelectedSort] = useState('')
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
+
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
+  }
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a, b) => a[sort.localeCompare(b[sort])]))
+
+  }
+
 
   return (
     <div className="App">
-      <form>
-        <MyInput type='text' placeholder='Post name' />
-        <MyInput type='text' placeholder='Post description' />
-        <MyButton disabled>Create Post</MyButton>
-      </form>
-      <PostList posts={posts} title='Posts about JS' />
+      <PostForm create={createPost} />
 
+      <hr style={{ margin: '15px 0' }} />
+
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="sorting"
+          options={[
+            { value: 'title', name: 'By name' },
+            { value: 'body', name: 'By description' },
+          ]}
+        />
+
+      </div>
+      {posts.length !== 0
+        ?
+        <PostList remove={removePost} posts={posts} title='Posts about JS' />
+        :
+        <h1 style={{ textAlign: 'center' }}>
+          Posts not found!
+        </h1>
+      }
     </div>
   );
 }
